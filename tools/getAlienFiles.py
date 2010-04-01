756d7c8f813d699d25e5f0731f8c26a97ad54a85
Oystein Djuvsland <odjuvsla@pr-djuvsland.localdomain>
2010-04-01 21:49:39 +0200

    - changes...

--- tools/getAlienFiles.py
+++ tools/getAlienFiles.py
@@ -1,52 +1,146 @@
+#!/usr/bin/env python
 import sys
 import os
+import threading
 
-aliensh = "/home/odjuvsla/Workspace/alice/alien/api/bin/aliensh" # path to aliensh
-gridPath = " /alice/sim/PDC_09/LHC09a4/"                  # general path to data (in alien) (without run number)
-stdfileName = "AliESDs.root"                                          # Filename to download
+class DownloadFileThread(threading.Thread):
 
-runNb = sys.argv[1]                                             
-numberOfFolders = int(sys.argv[2])
+    def __init__(self, threadNumber):
+        threading.Thread.__init__(self)
+        self.threadNumber = threadNumber
 
-path = gridPath + runNb
+    def run(self):
+        
+        alienshCommand =  "aliensh file:alscript" + repr(self.threadNumber) + " > status" + repr(self.threadNumber) + " &"
+    #    os.system(alienshCommand)
+        print alienshCommand
 
-tmpFilename = "tmp_" + runNb +".txt"
-scriptFilename ="script_" + runNb + ".txt"
+class Downloader():
+    
+    def __init__(self, filepath, destdir, nthreads=1):
 
-if os.system("ls " + runNb) != 0:
-    command = "mkdir " + runNb
-    os.system(command)
+        self.filepath = str(filepath)
+        self.nthreads = int(nthreads)
+        self.destdir = destdir
 
-command = aliensh + " -c 'ls " + path + "' > " + tmpFilename
+        self.pathParts = []
+        self.scanFilepath()
+        self.makeScript()
+
+    def start(self):
+
+#        for i in range(self.nthreads):
+#            t = DownloadFileThread(i)
+#            t.start()
+
+        for i in range(self.nthreads):
+            alienshCommand =  "aliensh file:alscript" + repr(i)# + " > status" + repr(i) + " &"
+            os.system(alienshCommand)
+            print alienshCommand
+            
+    def scanFilepath(self):
+
+        cnt = 0
+        for part in self.filepath.split("*"):
+#            self.pathParts[cnt] = part
+            self.pathParts.append(part)
+
+        print len(self.pathParts)
 
 
-os.system(command)
-
-tmpfile = open(tmpFilename, "r")
-
-folderNb = -1
-
-scriptFile = open(scriptFilename, "w")
-
-dirname = ""
-
-n = 0
-while 1:
-    folderNb = tmpfile.readline()
-    if folderNb == "" :
-        break
-    dirname =  runNb + "/" + folderNb.strip()
-    if os.system("ls " + dirname) != 0:
-        command = "mkdir " + dirname
+    def makeScript(self):
+        
+        command = "aliensh -c \"find " + self.filepath + " AliESDs.root\" > lstemp "
         os.system(command)
-    command = "cp " + path + "/" + folderNb.strip() + "/" + stdfileName+ " file:" + dirname + "/" + stdfileName+ " \n"
-    scriptFile.write(command)
-    n = n+1
-    if n > numberOfFolders:
-        break
-tmpfile.close()
-scriptFile.close()    
+        print command
+        F = open("lstemp", "r")
+        lines = F.readlines()
+        
+        runNb = 0
+        gdc = 0
+        cnt = 0
+        os.system("rm alscript*")
 
-command = aliensh + " file:" + scriptFilename
-os.system(command)
-#print command
+        for l in lines:
+
+            dirname = (l.split("/"))[-2:-1]
+            if dirname:
+                cnt = cnt + 1
+
+        filesPerThread = cnt/self.nthreads + 1
+
+        print "# of files: " + repr(cnt) + ", # of threads: " + repr(self.nthreads) + " -> " + repr(filesPerThread) + " files per thread"
+
+        cnt = 0
+        threadNumber = 0
+        for l in lines:
+
+            dirname = (l.split("/"))[-2:-1]
+            if dirname:
+#                print dirname
+                #print (dirname[2:11])
+                runNb = int((dirname[0])[2:11])
+                gdc = int((dirname[0].split(".")[0])[-3:])
+                part = int((dirname[0].split(".")[1]))
+
+#                print "Run #: " + repr(runNb)
+#                print "GDC #: " + repr(gdc)
+#                print "Part #: " + repr(part)
+                
+                finalDestination = self.destdir + "/" + repr(runNb) + "/" + repr(gdc) + "/" + repr(part)
+
+                mkdir = "mkdir -p " + finalDestination
+
+                downcommand = "cp " + l.rstrip(" \n") + " file:" + finalDestination + "/."
+
+                os.system(mkdir)
+                
+                threadNumber = cnt/filesPerThread
+
+#                print "File #: " + repr(cnt) + ", thread #: " + repr(threadNumber)
+
+                f = open("alscript" + repr(threadNumber), "a")
+                f.write(downcommand + "\n")
+                f.close()
+                
+                cnt = cnt + 1
+                
+        nthreads = threadNumber + 1
+        if self.nthreads != nthreads:
+
+            print "Changed the number of threads from " + repr(self.nthreads) + " to " + repr(nthreads)
+            self.nthreads = nthreads
+
+
+from optparse import OptionParser
+
+
+parser = OptionParser()
+
+parser.add_option("-f", "--file", dest="filepath",
+                  help="The path to the file(s) to get from alien. Accepts \"*\"s as wildcards")
+parser.add_option("-d", "--destination", dest="destination",
+                  help="The destination for the download")
+parser.add_option("-n", "--nthreads", dest="nthreads",
+                  help="Number of download threads to run")
+
+(options, args) = parser.parse_args()
+
+nthreads = 1
+
+if not options.filepath:
+    print "You have to specify a filepath to download! Idiot!"
+    exit(-1)
+if not options.destination:
+    print "You have to specify a destination to download to! Idiot!"
+    exit(-1)
+if options.nthreads:
+    nthreads = options.nthreads
+
+downloader = Downloader(options.filepath, options.destination, nthreads)
+
+downloader.start()
+
+
+
+
